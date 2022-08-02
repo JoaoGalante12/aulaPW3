02829d1a0fd138cd5753c28b0fb92088e9e04226
João Galante <101640788+JoaoGalante12@users.noreply.github.com>
2022-08-02 08:46:30 -0300

    Add files via upload

---
+++ Alunos.php
@@ -0,0 +1,5 @@
+<?php
+require 'aluno.php';
+    $aluno1 = new aluno("lucas", 5, 7, 9);
+
+?>
---
+++ laços.php
@@ -0,0 +1,14 @@
+<?php
+require 'aluno.php';
+
+$nomes = array(
+    array('nome' => $nome);
+    array('nota1' => $nota1);
+    array('nota2' => $nota2);
+    array('nota3' => $nota3);
+); 
+
+for() {
+
+}
+?>
